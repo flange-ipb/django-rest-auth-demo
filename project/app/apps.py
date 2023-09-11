fad0bcf88d31446e3ccec4ac2754a7fa4e4c02d3
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2023-09-11 16:48:48 +0200

    correct broken imports (leftovers from experimentation)

--- project/app/apps.py
+++ project/app/apps.py
@@ -1,8 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals import pre_save
-
-from project import settings
-from project.app.signals import uuid_as_username
 
 
 class AppConfig(AppConfig):
