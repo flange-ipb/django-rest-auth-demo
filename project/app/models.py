f04aa20c268597bb4430fd2accacf553af6c9938
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2023-09-15 15:58:31 +0200

    extract ORCID from social login data and add it to the user profile

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,7 @@
+import allauth
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(allauth.app_settings.USER_MODEL, on_delete=models.CASCADE)
+    orcid = models.CharField(max_length=19)
--- project/app/signals.py
+++ project/app/signals.py
@@ -1,8 +1,13 @@
 import uuid
 
+from allauth.account.signals import user_signed_up
+from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.providers.orcid.provider import extract_from_dict
 from django.conf import settings
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
+from project.app.models import UserProfile
 
 
 @receiver(pre_save, sender=settings.AUTH_USER_MODEL)
@@ -10,3 +15,26 @@ def uuid_as_username(sender, instance, **kwargs):
     if not instance.username:
         instance.username = str(uuid.uuid4())
         # TODO: consider https://docs.python.org/3/library/uuid.html#uuid.UUID.urn later ...
+
+
+# Signal 'social_account_added' is not sent when a social login creates a new user,
+# see https://github.com/pennersr/django-allauth/issues/1781
+#
+# Not sure if it's wise to also update the ORCID every time an existing user logs
+# in or connects to the social provider ...
+@receiver(user_signed_up)
+def add_orcid_when_user_is_created_by_orcid_login(sender, request, user, **kwargs):
+    social_account: SocialAccount = user.socialaccount_set.filter(provider="orcid").first()
+    if not social_account:
+        return
+
+    orcid = extract_orcid_from_data(social_account.extra_data)
+    if not orcid:
+        return
+
+    user_profile = user.userprofile if hasattr(user, "userprofile") else UserProfile(user=user, orcid=orcid)
+    user_profile.save()
+
+
+def extract_orcid_from_data(data):
+    return extract_from_dict(data, ["orcid-identifier", "path"])
